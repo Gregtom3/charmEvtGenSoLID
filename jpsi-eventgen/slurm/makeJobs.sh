d32571cd1b0eced55695aa31a14f7be3101f9de7
Gregory Matousek <gmat@ifarm1801.jlab.org>
2021-07-21 13:48:36 -0400

    makeJobs scripts

--- jpsi-eventgen/slurm/makeJobs.sh
+++ jpsi-eventgen/slurm/makeJobs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-workdir="/work/halla/solid/gam62/Research/charmEvtGenSoLID/psi2S-eventgen"
+workdir="/work/halla/solid/gam62/Research/charmEvtGenSoLID/jpsi-eventgen"
 
-beamE=(11 12 15 17 24)
+beamE=(9 10 11 12 17 24)
 nuc=(0 1)
 nuc_str=("p" "D")
 type=("photo" "electro")
-n=(5000000 100000000)
+n=(500000 10000000)
 
 runJobs="${workdir}/slurm/runJobs.sh"
 touch $runJobs
@@ -25,12 +25,12 @@ do
 	    echo "#SBATCH --account=halla" >> $file
 	    echo "#SBATCH --partition=production" >> $file
 	    echo "#SBATCH --mem-per-cpu=5000" >> $file
-	    echo "#SBATCH --job-name=psi2S_${nuc_str[${i}]}_${type[${j}]}_${energy}" >> $file
+	    echo "#SBATCH --job-name=jpsi_${nuc_str[${i}]}_${type[${j}]}_${energy}" >> $file
 	    echo "#SBATCH --time=01:00:00" >> $file
 	    echo "#SBATCH --chdir=${workdir}" >> $file
 	    echo "#SBATCH --output=${workdir}/slurm/output/%x-%j-%N.out" >> $file
 	    echo "#SBATCH --error=${workdir}/slurm/output/%x-%j-%N.err" >> $file
-	    echo "root -l ${type[${j}]}_solid_study_psi2S.C\\(${energy},${nuc[${i}]},${n[${j}]}\\)" >> $file
+	    echo "root -l ${type[${j}]}_solid_study_jpsi.C\\(${energy},${nuc[${i}]},${n[${j}]}\\)" >> $file
 
 	    echo "sbatch shells/${nuc_str[${i}]}_${type[${j}]}_$energy.sh" >> $runJobs
 	done 
